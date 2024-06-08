0ce4b4203aa1083f54691db47211790cda65372c
afgomezv <afgomezvonline@gmail.com>
2024-06-07 21:08:28 -0500

    bases angular

---
+++ src/app/heroes/list/list.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-list',
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.css',
+})
+export class ListComponent {
+  public heroNames: string[] = [
+    'Spiderman',
+    'IronMan',
+    'Hulk',
+    'Thor',
+    'Deadpool',
+    'Doctor Strange ',
+  ];
+
+  public deleteHero?: string;
+
+  removeLastHero(): void {
+    this.deleteHero = this.heroNames.pop();
+  }
+}
